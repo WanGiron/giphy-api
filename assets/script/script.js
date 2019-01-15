746f8f164158ffe391f6670d23771d29451956f6
William and Lady Navas <ladysantoyo@Williams-MacBook-Pro.local>
2019-01-14 21:11:13 -0500

    version 1.7

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,15 @@
 
 
 // global variables
-var fruits = ['mango', 'grape', 'orange'];
+var fruits = ['mango', 'grape', 'orange', 'coconut'];
 console.log(fruits);
+var frutaMia = [];
+
 
 // function to generate gif from API when buttons are pressed    key=dhLFlW8aHxB3KJixF0PIyztPiyCqOqgq
 function getData(){     
 var input = $(this).attr("data-name");        
 var queryURL = "https://api.giphy.com/v1/gifs/search?q="+input+"+&api_key=dhLFlW8aHxB3KJixF0PIyztPiyCqOqgq&limit=10";
-
 $.ajax({
 url: queryURL,
 method: "GET"
@@ -19,11 +20,12 @@ method: "GET"
     // generating the img and appending to DOM
     var newGif = response.data;
     for(i in newGif){
-     $("#fruitGifBox").append("<img src='"+newGif[i].images.original.url+" 'style='width: 200px;'/>");   
+     $("#fruitGifBox").append("<img src=' "+newGif[i].images.original.url+" 'style='width: 200px;' class='animated gif" + newGif[i].id + "' data-gif='" + newGif[i].id + "' />");
+     $("#fruitGifBox").append("<img src=' "+newGif[i].images.original_still.url+" 'style='width: 200px;' class='still gif" + newGif[i].id + "' data-gif='" + newGif[i].id + "' />"); 
+     hideAnimated();  
     }
 }); 
 }
-
 
 // function to generate the  fruit buttons from the array
 function fruitButtons(){ 
@@ -50,8 +52,39 @@ $("#getFruit").on("click", function(event){
     fruits.push(newFruit);
     // run function to create buttons to main-box div
     fruitButtons();
-})  
+}) 
+
+// to hide and show img function
+function hideStill (){
+    $(".still").hide();
+}
+
+function hideAnimated (){
+    $(".animated").hide();
+}
+
+
+function animate(){
+    $(".still.gif" + $(this).attr("data-gif")).hide();
+    $(".animated.gif" + $(this).attr("data-gif")).show();
+    console.log($(this).parent().html());
+}
+
+function freeze (){
+    $(".animated.gif" + $(this).attr("data-gif")).hide();
+    $(".still.gif" + $(this).attr("data-gif")).show();
+}
+
+
+
 
 fruitButtons();
+
 // run function on click that calls the gif from API according to buttton assigned value
+$(".gifButtonAnimated").on("click", ".fruit-t=btn", getData);
+
 $(document).on("click", ".fruit-btn", getData);
+
+// on click function gif to animate and stop
+$(document).on("click", ".still", animate);
+$(document).on("click", ".animated", freeze);
